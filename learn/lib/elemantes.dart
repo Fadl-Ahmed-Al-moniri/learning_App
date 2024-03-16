a034fd77ec62d44dbb62e08b6bf70c8f673cd7b3
Fadl-Ahmed-Al-moniri <fadlahmedalmoniri@gmail.com>
2024-03-16 20:16:31 +0300

    Update_2

--- learn/lib/elemantes.dart
+++ learn/lib/elemantes.dart
@@ -1,37 +1,40 @@
 import 'package:flutter/material.dart';
 
 Widget mytext(String txt,
-    {Color mycolor = Colors.white, String myfontFamily = "Marhey"}) {
+    {Color mycolor = Colors.white,
+    String myfontFamily = "Blabeloo-font-v.1.0",
+    double mysize = 26}) {
   return Text(
     txt,
+    textAlign: TextAlign.right,
+    selectionColor: Colors.red,
     style: TextStyle(
-        fontWeight: FontWeight.w600,
-        fontSize: 20,
+        // fontWeight: FontWeight.w600,
+        fontSize: mysize,
+        decorationColor: Colors.red,
+        leadingDistribution: TextLeadingDistribution.proportional,
         fontFamily: myfontFamily,
         color: mycolor),
   );
 }
 
 Widget mybottom(String txt2, {VoidCallback? tap}) {
-  return Container(
-    height: 50,
-    width: 140,
-    decoration: const BoxDecoration(
-      gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 57, 134, 252),
-            Color.fromARGB(255, 49, 120, 228),
-          ],
-          begin: AlignmentDirectional.topCenter,
-          end: AlignmentDirectional.bottomCenter,
-          stops: [0.5, 0.4]),
-      borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-          bottomLeft: Radius.circular(20),
-          bottomRight: Radius.circular(20)),
-    ),
-    child: MaterialButton(
-        onPressed: tap, textColor: Colors.white, child: mytext(txt2)),
-  );
+  return MaterialButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      color: const Color.fromARGB(255, 0, 100, 255),
+      height: 68.38,
+      minWidth: 166.4,
+      onPressed: tap,
+      textColor: Colors.white,
+      child: Text(
+        txt2,
+        textAlign: TextAlign.right,
+        style: const TextStyle(
+            // fontWeight: FontWeight.w600,
+            fontSize: 27,
+            decorationColor: Colors.red,
+            leadingDistribution: TextLeadingDistribution.proportional,
+            fontFamily: "Blabeloo-font-v.1.0",
+            color: Colors.white),
+      ));
 }
--- learn/lib/main.dart
+++ learn/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:easy_stepper/easy_stepper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:learn/custom_button.dart';
 import 'package:learn/elemantes.dart';
 
 void main() {
@@ -13,7 +16,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -29,96 +31,293 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentStep = 0;
+  int activeStep = 0;
+  int activeStep2 = 0;
+  int reachedStep = 0;
+  int upperBound = 5;
+  double progress = 0.2;
+  Set<int> reachedSteps = <int>{0, 2, 4, 5};
+  final Widgetes = <Widget>[
+    Container(
+      color: Colors.white,
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Expanded(
+              child: SizedBox(
+                height: 281,
+                width: 360,
+              ),
+            ),
+            mytext("تعرف علينا", mycolor: Colors.red, mysize: 25),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: mytext(
+                  "عالم الحروف تطبيق تعليمي تفاعلي يهدف لدمج التعلم بالترفيه",
+                  mycolor: Colors.blue,
+                  myfontFamily: "Monadi",
+                  mysize: 24),
+            )
+          ]),
+    ),
+    Container(
+      color: Colors.white,
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: SizedBox(
+                height: 281,
+                width: 360,
+                child: Image.asset(
+                  r"images\bef0c802019aaab8dd99f5457204e7e2.gif",
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            mytext("مالذي يميزنا؟", mycolor: Colors.red, mysize: 25),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: mytext(
+                  "تطوير مهارات التعرف على الحروف الابجدية بطريقة ممتعة وتفاعلية لزرع حب التعلم ",
+                  mycolor: Colors.blue,
+                  myfontFamily: "Monadi",
+                  mysize: 24),
+            )
+          ]),
+    ),
+    Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Image.asset(
+                    r"images\927bdcf59e5a5d073e6e2086d82f2e0a.png",
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Container(
+                  width: 171,
+                  margin: const EdgeInsets.only(bottom: 15),
+                  height: 266,
+                  child: Image.asset(
+                    r"images\3578009246170d48c2abc7d0f2bb09b9.png",
+                    fit: BoxFit.fill,
+                  ),
+                )
+              ],
+            ),
+          ),
+          mytext("تحب المغامرة  ؟", mycolor: Colors.red),
+          const Padding(
+            padding: EdgeInsets.only(top: 5, right: 10),
+          ),
+          mytext("شارك في مغامرة التعرف على الحروف الابجدية مع ماجد و حنين ",
+              mycolor: Colors.blue, myfontFamily: "Monadi", mysize: 24)
+        ]),
+  ];
+
+  void increaseProgress() {
+    if (progress < 1) {
+      setState(() => progress += 0.2);
+    } else {
+      setState(() => progress = 0);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     double screeenheight = MediaQuery.of(context).size.height;
     double screeenwidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 35),
-        child: Stepper(
-          currentStep: _currentStep,
-          connectorThickness: 3,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(children: [
+            EasyStepper(
+              activeStep: activeStep,
+              lineStyle: const LineStyle(
+                lineLength: 61,
+                lineType: LineType.normal,
+                lineThickness: 4,
+                lineSpace: 0,
+                lineWidth: 10,
+                defaultLineColor: const Color.fromARGB(200, 169, 206, 210),
+                finishedLineColor: const Color.fromARGB(255, 56, 133, 252),
+                unreachedLineType: LineType.dashed,
+              ),
+              stepShape: StepShape.rRectangle,
+              stepBorderRadius: 50,
+              borderThickness: 11,
+              internalPadding: 0,
+              padding: const EdgeInsetsDirectional.symmetric(
+                horizontal: 35,
+                vertical: 30,
+              ),
+              stepRadius: 35,
+              finishedStepBorderColor: const Color.fromARGB(255, 56, 133, 252),
+              showStepBorder: true,
+              finishedStepTextColor: const Color.fromARGB(255, 56, 133, 252),
+              finishedStepBackgroundColor:
+                  const Color.fromARGB(255, 169, 206, 210),
+              activeStepIconColor: Colors.deepOrange,
+              activeStepBorderColor: const Color.fromARGB(255, 56, 133, 252),
+              unreachedStepBackgroundColor:
+                  const Color.fromARGB(200, 169, 206, 210),
+              activeStepBackgroundColor:
+                  const Color.fromARGB(255, 169, 206, 210),
+              unreachedStepBorderColor: const Color.fromARGB(150, 56, 133, 252),
+              showLoadingAnimation: false,
+              defaultStepBorderType: BorderType.normal,
+              steps: [
+                EasyStep(
+                  customStep: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Opacity(
+                      opacity: activeStep >= 0 ? 1 : 0.3,
+                      child: mytext("1", myfontFamily: "DG-3asomy-Regular"),
+                    ),
+                  ),
+                ),
+                EasyStep(
+                  customStep: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Opacity(
+                      opacity: activeStep >= 1 ? 1 : 0.3,
+                      child: mytext("2", myfontFamily: "DG-3asomy-Regular"),
+                    ),
+                  ),
+                ),
+                EasyStep(
+                  customStep: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Opacity(
+                      opacity: activeStep >= 2 ? 1 : 0.3,
+                      child: mytext("3", myfontFamily: "DG-3asomy-Regular"),
+                    ),
+                  ),
+                ),
+              ],
+              onStepReached: (index) => setState(() => activeStep = index),
+            ),
+            SizedBox(
+              height: screeenheight / 1.7,
+              width: screeenwidth / 1.2,
+              child: Widgetes[activeStep],
+            ),
+            CustomButton(
+              navigateto: (context) => Widgetes[activeStep],
+            )
+            // activeStep == 2
+            //     ? mybottom("هيا لنبدأ", tap: () => Widgetes[activeStep])
+            //     :
 
-          elevation: 0,
-
-          //  connectorColor: MaterialStateProperty.resolveAs(Colors.accents, Colors.accents),
-
-          type: StepperType.horizontal,
-          steps: getSteps(),
-          onStepContinue: () {
-            _currentStep == getSteps().length - 1
-                ? null
-                : setState(() {
-                    _currentStep += 1;
-                  });
-          },
-
-          // onStepCancel: () {
-          //   _currentStep == getSteps().first
-          //       ? null
-          //       : setState(() {
-          //           _currentStep -= 1;
-          //         });
-          // },
-          controlsBuilder: (context, onStepContinue) {
-            return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 120),
-                child: mybottom("التالي", tap: onStepContinue.onStepContinue));
-          },
+            // mybottom("التالي", tap: () => Widgetes[activeStep])
+          ]),
         ),
+        backgroundColor: Colors.white,
       ),
     );
+
+    // return Scaffold(
+    //   body: Padding(
+    //     padding: const EdgeInsets.only(top: 35),
+    //     child: Stepper(
+    //       currentStep: _currentStep,
+    //       connectorThickness: 3,
+
+    //       elevation: 0,
+
+    //       //  connectorColor: MaterialStateProperty.resolveAs(Colors.accents, Colors.accents),
+
+    //       type: StepperType.horizontal,
+    //       steps: getSteps(),
+    //       onStepContinue: () {
+    //         _currentStep == getSteps().length - 1
+    //             ? null
+    //             : setState(() {
+    //                 _currentStep += 1;
+    //               });
+    //       },
+
+    //       // onStepCancel: () {
+    //       //   _currentStep == getSteps().first
+    //       //       ? null
+    //       //       : setState(() {
+    //       //           _currentStep -= 1;
+    //       //         });
+    //       // },
+    //       controlsBuilder: (context, onStepContinue) {
+    //         return Padding(
+    //             padding: const EdgeInsets.symmetric(horizontal: 120),
+    //             child: mybottom("التالي", tap: onStepContinue.onStepContinue));
+    //       },
+    //     ),
+    //   ),
+    // );
   }
 
-  List<Step> getSteps() => [
-        Step(
-            title: _currentStep == 0
-                ? Image.asset(
-                    r"images\1710454976805.png",
-                    fit: BoxFit.contain,
-                  )
-                : const Text(""),
-            content: Container(
-              height: MediaQuery.of(context).size.height / 1.4,
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    mytext("تعرف علينا", mycolor: Colors.red),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30, right: 10),
-                    ),
-                    mytext(
-                        "عالم الحروف تطبيق تعليمي تفاعلي يهدف لدمج التعلم بالترفيه",
-                        mycolor: Colors.blue,
-                        myfontFamily: "")
-                  ]),
-            ),
-            isActive: _currentStep >= 0),
-        Step(
-            title: _currentStep == 1
-                ? Image.asset(
-                    r"images\1710454976805.png",
-                    fit: BoxFit.contain,
-                  )
-                : const Text(""),
-            content: Container(),
-            isActive: _currentStep >= 1),
-        Step(
-            title: _currentStep == 2
-                ? Image.asset(
-                    r"images\1710454976805.png",
-                    fit: BoxFit.contain,
-                  )
-                : const Text(""),
-            content: Container(),
-            isActive: _currentStep >= 2),
-      ];
+  // List<Step> getSteps() => [
+  //       Step(
+  //           title: _currentStep == 0
+  //               ? Container(
+  //                   height: 127,
+  //                   width: 169,
+  //                   child: Image.asset(
+  //                     r"images\2bc79d0ed8f4866038405ea83d313fd9.gif",
+  //                     fit: BoxFit.fill,
+  //                   ),
+  //                 )
+  //               : const Text(""),
+  // content: Container(
+  //   height: MediaQuery.of(context).size.height / 1.4,
+  //   width: MediaQuery.of(context).size.width,
+  //   child: Column(
+  //       crossAxisAlignment: CrossAxisAlignment.end,
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //       children: [
+  //         mytext("تعرف علينا", mycolor: Colors.red),
+  //         const Padding(
+  //           padding: EdgeInsets.only(top: 30, right: 10),
+  //         ),
+  //         mytext(
+  //             "عالم الحروف تطبيق تعليمي تفاعلي يهدف لدمج التعلم بالترفيه",
+  //             mycolor: Colors.blue,
+  //             myfontFamily: "Monadi")
+  //       ]),
+  // ),
+  //           isActive: _currentStep >= 0),
+  //       Step(
+  //           title: _currentStep == 1
+  //               ? Image.asset(
+  //                   r"images\2bc79d0ed8f4866038405ea83d313fd9.gif",
+  //                   fit: BoxFit.contain,
+  //                 )
+  //               : const Text(""),
+  //           content: Container(),
+  //           isActive: _currentStep >= 1),
+  //       Step(
+  //           title: _currentStep == 2
+  //               ? Image.asset(
+  //                   r"images\2bc79d0ed8f4866038405ea83d313fd9.gif",
+  //                   fit: BoxFit.contain,
+  //                 )
+  //               : const Text(""),
+  //           content: Container(),
+  //           isActive: _currentStep >= 2),
+  //     ];
   // Widget mybottom() {
   //   return Container(
   //     height: 80,
